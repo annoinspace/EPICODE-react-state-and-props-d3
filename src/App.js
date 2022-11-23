fe05182a730e6653d6ece30e12026fd6e2ffa6f4
annoinspace <aneesahalmask@gmail.com>
2022-11-23 13:27:44 +0000

    alert added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+import WarningSign from "./components/WarningSign"
 
 function App() {
-  return <div></div>
+  return (
+    <div className="App">
+      <WarningSign text="YOU ARE IN DANGER!!!!!!" />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/WarningSign.jsx
@@ -0,0 +1,11 @@
+import { Alert } from "react-bootstrap"
+
+const WarningSign = (props) => {
+  return (
+    <div>
+      ;<Alert variant="danger">{props.text}</Alert>
+    </div>
+  )
+}
+
+export default WarningSign
