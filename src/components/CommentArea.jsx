344697230a8042acfaff720d06a03b6210bb54e1
annoinspace <aneesahalmask@gmail.com>
2022-11-24 14:50:46 +0000

    comments removed

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -12,6 +12,7 @@ class CommentArea extends Component {
         `https://striveschool-api.herokuapp.com/api/comments/${this.props.bookId}`,
         {
           headers: {
+            method: "GET",
             Authorization:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzczYTZmMTNhN2ZjNDAwMTU5N2VjMzQiLCJpYXQiOjE2NjkyOTYxNDgsImV4cCI6MTY3MDUwNTc0OH0.W76RGzP36ffco5AYQ_-N4bajwbw6S-r_NidO0S-fn2M"
           }
@@ -41,57 +42,5 @@ class CommentArea extends Component {
     )
   }
 }
-
-// class CommentArea extends Component {
-//   state = {
-//     comments: []
-//   }
-
-//   fetchComments = async () => {
-//     try {
-//       let response = await fetch(
-//         `https://striveschool-api.herokuapp.com/api/comments/${this.props.bookId}`,
-//         {
-//           headers: {
-//             method: "GET",
-//             "Content-Type": "application/json",
-//             Authorisation:
-//               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzczYTZmMTNhN2ZjNDAwMTU5N2VjMzQiLCJpYXQiOjE2NjkyOTYxNDgsImV4cCI6MTY3MDUwNTc0OH0.W76RGzP36ffco5AYQ_-N4bajwbw6S-r_NidO0S-fn2M"
-//           }
-//         }
-//       )
-//       if (response.ok) {
-//         let data = await response.json()
-//         console.log(data)
-//         this.setState({ comments: data })
-//       } else {
-//         console.log("error fetching the comments :(")
-//       }
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
-
-//   componentDidMount() {
-//     this.fetchComments()
-//   }
-
-//   render() {
-//     console.log(this.props.bookId)
-//     return (
-//       <div>
-//         <h5>Comments</h5>
-//         <ListGroup>
-//           {this.state.comments.map((comment) => (
-//             <ListGroup.Item key={comment._id}>{comment.comment}</ListGroup.Item>
-//           ))}
-//           {this.state.isError && (
-//             <Alert variant="danger">Whoopsie, something went wrong! :(</Alert>
-//           )}
-//         </ListGroup>
-//       </div>
-//     )
-//   }
-// }
 
 export default CommentArea
